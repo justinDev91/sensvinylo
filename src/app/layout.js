88a620f5b987a6857d2a44c998b6085c16bec373
justinDev91 <justin@sustainsoft.eu>
2024-07-08 17:11:43 +0200

    feat : add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,43 +2,61 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
-  const metadata = {
-    title: "Magasin de Vinyles - Achetez des vinyles classiques et des nouveautés",
-    description: "Bienvenue sur notre magasin de vinyles ! Découvrez une large gamme de vinyles, des classiques aux dernières sorties.",
+export const metadata = {
+  siteName: 'sensvinylo',
+  generator: 'Next.js',
+  applicationName: 'Next.js',
+  title: "sensvinylo - Achetez des vinyles classiques et des nouveautés",
+  description: "Bienvenue sur notre sensvinylo ! Découvrez une large gamme de vinyles, des classiques aux dernières sorties.",
+  url: "https://www.sensvinylo-musique.fr",
+  image: "https://www.sensvinylo-musique.fr/vinyle-img.png",
+  twitterUsername: "@sensvinylo",
+  keywords: ['vinyles', 'musique', 'albums', 'classiques', 'nouvelles sorties'],
+  authors: [{ name: 'Justin' }, { name: 'Katasi', url: 'https://www.sensvinylo-musique.fr' }],
+  openGraph: {
+    title: 'sensvinylo',
+    description: "Bienvenue sur notre sensvinylo ! Découvrez une large gamme de vinyles, des classiques aux dernières sorties.",
     url: "https://www.sensvinylo-musique.fr",
-    image: "https://www.sensvinylo-musique.fr/vinyle-img.png",
-  };
+    siteName: 'Sensvinylo',
+    locale: 'fr_FR',
+    type: 'website',
+  },
+  alternates: {
+    canonical: 'https://www.sensvinylo-musique.fr',
+    languages: {
+      'fr_FR': '/fr_FR',
+    },
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+
+
+};
+
+export default function RootLayout({ children }) {
 
   return (
     <>
       <Head>
-      <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width" />
-        <title>{metadata.title}</title>
-        <meta name="robots" content="index,follow" />
-        <meta name="description" content={metadata.description} />
-        <meta name="twitter:site" content="@sensvinylo" />
-        <meta name="twitter:creator" content="@sensvinylo" />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:url" content={metadata.url} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:alt" content="Magasin de Vinyles - Achetez des vinyles classiques et des nouveautés" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="628" />
-        <meta property="og:locale" content="fr_FR" />
-        <meta property="og:site_name" content="Magasin de Vinyles" />
-        <link rel="canonical" href={metadata.url} />
-        <meta name="keywords" content="vinyles, musique, albums, classiques, nouvelles sorties" />
         <script type="application/ld+json">
           {JSON.stringify({
             "@context": "https://schema.org",
-            "@type": "LocalBusiness",
-            "name": "Magasin de Vinyles",
+            "@type": "website",
+            "name": "sensvinylo",
             "address": {
               "@type": "PostalAddress",
               "streetAddress": "242 Rue du Faubourg Saint-Antoine",
