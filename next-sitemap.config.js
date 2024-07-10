19286660b5fdbe3812f0fd50c678c43856add51b
Erwann Jouvet <vinistys@gmail.com>
2024-07-10 08:49:24 +0200

    modif sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,5 +5,19 @@ module.exports = {
     sitemapSize: 7000,
     changefreq: 'daily',
     priority: 0.7,
+    additionalPaths: async (config) => [
+      {
+        loc: '/', // URL de la page d'accueil
+        changefreq: 'daily', // Fréquence de changement spécifique pour la page d'accueil
+        priority: 1.0, // Priorité spécifique pour la page d'accueil
+        lastmod: new Date().toISOString(), // Date de dernière modification
+      },
+      {
+        loc: '/about', // URL de la page 1
+        changefreq: 'daily', // Fréquence de changement spécifique
+        priority: 0.8, // Priorité spécifique pour cette page
+        lastmod: new Date().toISOString(), // Date de dernière modification
+      },
+    ],
   };
   
